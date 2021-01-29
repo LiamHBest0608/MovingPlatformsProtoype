a06e7645abbd02a52970802646e4471dc0401c04
liam @ alfacraft <liam@hillefors.se>
2021-01-29 15:20:54 +0100

    Initial commit

---
+++ src/main/java/me/liamhbest/MovingPlatforms.java
@@ -0,0 +1,54 @@
+package me.liamhbest;
+
+import me.liamhbest.data.PlatformsData;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public final class MovingPlatforms extends JavaPlugin {
+
+    private static MovingPlatforms instance;
+    public static MovingPlatforms getInstance(){return instance;}
+
+    FileConfiguration config;
+    File cfile;
+
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("MovingPlatforms is being enabled!");
+
+        commands();
+        listeners();
+
+        PlatformsData.setup();
+        PlatformsData.get().options().copyDefaults(true);
+        PlatformsData.save();
+
+        getConfig().options().copyDefaults(true);
+        cfile = new File(getDataFolder(), "config.yml");
+        instance = this;
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getLogger().info("MovingPlatforms is being disabled!");
+    }
+
+    public void listeners(){
+        PluginManager manager = getServer().getPluginManager();
+    }
+
+    public void commands(){
+
+    }
+}
+
+
+
+
+
+
+
---
+++ src/main/java/me/liamhbest/data/PlatformsData.java
@@ -0,0 +1,45 @@
+package me.liamhbest.data;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PlatformsData {
+
+    private static File file;
+    private static FileConfiguration customFile;
+
+    //Finds or generates the custom config file
+    public static void setup(){
+        file = new File(Bukkit.getServer().getPluginManager().getPlugin("MovingPlatformsPrototype").getDataFolder(), "platforms.yml");
+
+        if (!file.exists()){
+            try{
+                file.createNewFile();
+            }catch (IOException e){
+                //owww
+            }
+        }
+        customFile = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public static FileConfiguration get(){
+        return customFile;
+    }
+
+    public static void save(){
+        try{
+            customFile.save(file);
+        }catch (IOException e){
+            System.out.println("Couldn't save file");
+        }
+    }
+
+    public static void reload(){
+        customFile = YamlConfiguration.loadConfiguration(file);
+    }
+
+}
