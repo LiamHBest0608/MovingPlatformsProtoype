bec30833257540b88d18a8046468d3102e46402a
liam @ alfacraft <liam@hillefors.se>
2021-01-30 21:41:30 +0100

    Started working on the API

---
+++ src/main/java/me/liamhbest/MovingPlatformsAPI.java
@@ -0,0 +1,13 @@
+package me.liamhbest;
+
+import org.bukkit.Location;
+
+public class MovingPlatformsAPI {
+
+    public void createPlatform(Location startLoc, Location endLoc, String platformName, float platformSpeed){
+
+    }
+
+
+
+}
