19b98964b0d589013888a2acfb112017de8013b6
liam @ alfacraft <liam@hillefors.se>
2021-02-06 00:54:25 +0100

    Fixes for command

--- src/main/java/me/liamhbest/commands/PlatformCommand.java
+++ src/main/java/me/liamhbest/commands/PlatformCommand.java
@@ -4,6 +4,7 @@ import me.liamhbest.Color;
 import me.liamhbest.MovingPlatforms;
 import me.liamhbest.data.PlatformsData;
 import me.liamhbest.utils.CreatePlatform;
+import me.liamhbest.utils.SavePlatform;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -13,6 +14,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.io.IOException;
+
 public class PlatformCommand implements CommandExecutor {
 
     MovingPlatforms plugin;
@@ -20,7 +23,9 @@ public class PlatformCommand implements CommandExecutor {
         this.plugin = plugin;
     }
 
-    public static boolean setPos1;
+    public static boolean setPos1 = false;
+    public static Location locPos1;
+    public static Location locPos2;
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -41,23 +46,54 @@ public class PlatformCommand implements CommandExecutor {
                     if (args.length>1){
                         //more
                         if (args[1].equalsIgnoreCase("pos1")){
-                            setPos1=true;
-                            p.sendMessage("set pos 1");
-                            Location loc = new Location(
-                                    p.getWorld(),
-                                    p.getLocation().getX(),
-                                    p.getLocation().getY()-1,
-                                    p.getLocation().getZ()
-                            );
-                            loc.getBlock().setType(Material.GOLD_BLOCK);
+                            if (setPos1){
+                                p.sendMessage("you have already set position 1");
+                            } else {
+                                setPos1=true;
+                                p.sendMessage("set pos 1");
+                                locPos1 = new Location(
+                                        p.getWorld(),
+                                        p.getLocation().getX(),
+                                        p.getLocation().getY()-1,
+                                        p.getLocation().getZ()
+                                );
+
+                                locPos1.getBlock().setType(Material.LAPIS_BLOCK);
+                            }
 
                         } else if (args[1].equalsIgnoreCase("pos2")){
                             if (!setPos1){
                                 p.sendMessage("you have to set pos1 first!");
                             } else {
                                 //create the platform
-                                p.sendMessage("you have now set both positions");
-                                setPos1 = false;
+                                locPos2 = new Location(
+                                        p.getWorld(),
+                                        p.getLocation().getX(),
+                                        p.getLocation().getY()-1,
+                                        p.getLocation().getZ()
+                                );
+
+                                if (locPos2.getBlockX()==locPos1.getBlockX()||locPos1.getBlockZ()==locPos2.getBlockZ()){
+                                    if (locPos1.getY()==locPos2.getY()){
+                                        //correct
+                                        p.sendMessage("you have now set both positions");
+                                        locPos2.getBlock().setType(Material.LAPIS_BLOCK);
+                                        setPos1 = false;
+
+                                        try {
+                                            SavePlatform.savePlatform(locPos1, locPos2, "name", 0.1D, plugin);
+                                            CreatePlatform.createPlatform(locPos1, locPos2,p, "name", 0.1D, plugin);
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+
+                                    } else {
+                                        p.sendMessage("they have to be on the same y level!");
+                                    }
+
+                                } else {
+                                    p.sendMessage("you have to be able to draw a STRAIGHT line between the locations. you can not do diagonal!");
+                                }
                             }
 
                         } else {
